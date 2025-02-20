5a56053ab1cec599323d85de8333309f9ceb58fc
rvposo <gabrielraposo0@hotmail.com>
2025-02-20 12:56:59 -0400

    feat: start home with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import { useState } from "react";
+//import { useState } from "react";
 import "./App.css";
-import Tasks from "./components/Tasks"; //tasks component imported
-import AddTask from "./components/AddTask";
+//import Tasks from "./components/Tasks"; //tasks component imported
+//import AddTask from "./components/AddTask";
 const App = () => {
-  const [message, setMessage] = useState("test");
-  const [text, newText] = useState("sample name");
+ // const [message, setMessage] = useState("test");
+  //const [text, newText] = useState("sample name");
 
   return (
     <>
-  <div className="h-screen w-screen bg-slate-500 flex justify-center">
-  <AddTask />
-      <Tasks /> {/* tasks component called */}
-      <button
-        onClick={() => {
-          setMessage("TESTZAO");
-          newText("new text rn");
-        }}
-      >
-        {message}
-      </button>
-      <h1 className="text-red-500" >{text}</h1>
-      <h2>name2</h2>
+  <div className="h-screen w-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-[500px]">
+      <h1 className="text-3xl text-slate-100 font-bold text-center">Task Manager</h1>
+    </div>
+      
   </div>
     </>
   );
 };
 
 export default App;
-
-// to do: install tailwindcss packaged and configure it at style.css
